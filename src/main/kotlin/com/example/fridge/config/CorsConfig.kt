d7dc3fbe9d351de1f98a5318cea360516a6e0037
Saeed <pakdelniasaeed@gmail.com>
2023-12-14 23:17:16 +0330

    fixed swagger error

--- src/main/kotlin/com/example/fridge/config/CorsConfig.kt
+++ src/main/kotlin/com/example/fridge/config/CorsConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
             .allowedMethods("*")
             .allowedHeaders("*")
             .exposedHeaders("Authorization")
